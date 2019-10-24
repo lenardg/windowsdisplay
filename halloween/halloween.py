26acc2298c3b7e1abf55d54673a10b09e20f7137
Lenard Gunda <lenard@offbeat.fi>
2019-10-24 13:08:47 +0300

    Improved the halloween lights script with morning mode, debug mode, etc

--- halloween/halloween.py
+++ halloween/halloween.py
@@ -1,25 +1,50 @@
 #!/usr/bin/env python
 
+##############################################################
+# Halloween lights for window display
+# uses Blinkt! library
+#
+# by Lenard Gunda
+##############################################################
+
 import colorsys
 import time
-from random import randint
+from random import randint, choice
 import blinkt
 
 spacing = 360.0 / 16.0
 hue = 0
 
-blinkt.set_clear_on_exit()
-blinkt.set_brightness(1.0)
+# the colors we use for blinking
+blinkColors = [(255,255,255),(255,255,255),(255,255,255),(255,64,0),(192,0,255)]
 
-def blueLilac():
+# Set to True to see time values. Set to False (default) for normal operation
+debug = False
+
+# setup
+if not debug:
+    blinkt.set_clear_on_exit()
+    blinkt.set_brightness(1.0)
+
+########################
+# Functions
+########################
+
+# clear the lights
+def clear():
+    if not debug:
+        blinkt.clear()
+        blinkt.show()
+
+def colorfade(minSeconds, maxSeconds, shift = 0, multiplier = 1.0, fullRange = 30):
     start = time.time()
-    duration = randint(20,45)
+    duration = randint(minSeconds, maxSeconds)
     while True:
-        current = time.time() % 30
-        if current >= 15:
-            current = 30 - current
-        current = current * 3
-        hue = current + 240  #int(time.time() * 100) % 360
+        current = time.time() % fullRange
+        if current >= fullRange / 2:
+            current = fullRange - current
+        current = current * multiplier
+        hue = current + shift 
         for x in range(blinkt.NUM_PIXELS):
             h = ((hue) % 360) / 360.0
             r, g, b = [int(c*255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
@@ -31,30 +56,20 @@ def blueLilac():
         if time.time() - start > duration:
             break
  
+def blueLilac():
+    colorfade(20, 45, 240, 3.0)
+
 def orangeRed():
-    start = time.time()
-    duration = randint(20,45)
-    while True:
-        current = time.time() % 30
-        if current >= 15:
-            current = 30 - current
-        hue = current  #int(time.time() * 100) % 360
-        for x in range(blinkt.NUM_PIXELS):
-            h = ((hue) % 360) / 360.0
-            r, g, b = [int(c*255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
-            blinkt.set_pixel(x, r, g, b)
-
-        blinkt.show()
-        time.sleep(0.5)
-
-        if time.time() - start > duration:
-            break
-        
+    colorfade(20, 45)
+       
 def blink():
     start = time.time()
     duration = randint(3,8)
+
+    r, g, b = choice(blinkColors)
+
     while True:
-        blinkt.set_all(255,255,255)
+        blinkt.set_all(r, g, b)
         blinkt.show()
         time.sleep(0.01)
         blinkt.clear()
@@ -63,33 +78,101 @@ def blink():
         if time.time() - start > duration:
             break
 
+def lightshow(canBlink):
+    if canBlink:
+        blink()
+    blueLilac()
+    if canBlink:
+        blink()
+    orangeRed()
+
+########################
+# CONSTANTS
+########################
+STATE_IDLE = 0
+STATE_MORNING_BLINK = 7
+STATE_MORNING_NOBLINK = 6
+STATE_EVENING_BLINK = 18
+STATE_EVENING_NOBLINK = 20
+
+########################
+# MAIN
+########################
 try:
+    print("HALLOWEEN lights")
+    state = STATE_IDLE
+                # states: 
+                #   0 not running
+                #   6 morning run without blink
+                #   7 morning run with blink 
+                #  18 night run with blink
+                #  20 night run without blink
+
+    debugtime = 1571882400 
+
+    # begin state machine
     while True:
-        utcnow = time.gmtime()
-        helnow = time.localtime()
-        canBlink = True
-        if utcnow.tm_hour < 15:   # we do not turn on before 15:00 UTC
-            time.sleep(60)
+        if not debug:
+            now = time.time()
+        else:
+            now = debugtime
+            debugtime = debugtime + 60
+            if debugtime > 1571981600:
+                break
+
+        utcnow = time.gmtime(now)
+        helnow = time.localtime(now)
+
+        # state transitions
+        if state == STATE_IDLE:
+            if helnow.tm_hour >= 6 and utcnow.tm_hour < 5:
+                print("It's 6:00 local, we turn on")
+                state = STATE_MORNING_NOBLINK
+                continue
+            elif utcnow.tm_hour >= 15 and helnow.tm_hour > 15 and helnow.tm_hour < 23:
+                print("It's 15:00 UTC, we turn on")
+                state = STATE_EVENING_BLINK
+                continue
+            if not debug:
+                time.sleep(60)
+            else:
+                print("{0}:{1} idle".format(helnow.tm_hour, helnow.tm_min))
             continue
-        elif helnow.tm_hour >= 23: # at 23:00 local time we turn off
-            break
-        elif helnow.tm_hour >= 20: # after 20:00 local time we do not blink 
+        elif state == STATE_MORNING_NOBLINK:
+            if helnow.tm_hour > 6 or (helnow.tm_hour == 6 and helnow.tm_min > 45):
+                print("After 6:45 local, blinking can start")
+                state = STATE_MORNING_BLINK
+                continue
+        elif state == STATE_MORNING_BLINK:
+            if utcnow.tm_hour > 5 or (utcnow.tm_hour == 5 and utcnow.tm_min > 24):
+                print("It's after 5:24 UTC, turn off lights")
+                state = STATE_IDLE
+                clear()
+                continue
+        elif state == STATE_EVENING_BLINK:
+            if helnow.tm_hour >= 20:
+                print("After 20:00 local, blinking will stop now")
+                state = STATE_EVENING_NOBLINK
+        elif state == STATE_EVENING_NOBLINK:
+            if helnow.tm_hour > 23 or (helnow.tm_hour == 23 and helnow.tm_min > 15):
+                print("It's 23:15 local, turn off lights")
+                state = STATE_IDLE
+                clear()
+                continue
+
+        if state == STATE_MORNING_BLINK or state == STATE_EVENING_BLINK:
+            canBlink = True
+        else:
             canBlink = False
 
-        if canBlink:
-            blink()
-        
-        blueLilac()
-
-        if canBlink:
-            blink()
-
-        orangeRed()
+        if debug:
+            print("{0}:{1} Lightshow, canBlink={2}".format(helnow.tm_hour, helnow.tm_min, canBlink))
+        else:
+            lightshow(canBlink)
 
 except KeyboardInterrupt:
     print("Keyboard break received")
 
-blinkt.clear()
-blinkt.show()
+clear()
 print("THE END.")
 
